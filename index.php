ce7ef564deed2653262b05978b615fb926c2f1ea
andreymecheva <andreymecheva@hotmail.com>
2014-03-06 10:04:49 -0500

    tres

--- index.php
+++ index.php
@@ -2,27 +2,40 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Jquery events</title>
+	<title>Jquery events 201</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<link rel="stylesheet" href="day.css">
+	<link rel="stylesheet" href="style.css">
 
 </head>
 <body>
 <div class="container">
-	<h1>Welcome to my website</h1>
-	<button data-file="day">Day</button>
-	<button data-file="night">Night</button>
-	<script>
+<dl>
+	<dt>What are your hours?</dt>
+	<dd>We are open 24/7</dd>
+	<dt>What are your hours?</dt>
+	<dd>We are open 24/7</dd>
+	<dt>What are your hours?</dt>
+	<dd>We are open 24/7</dd>
+	<dt>What are your hours?</dt>
+	<dd>We are open 24/7</dd>
+	<dt>What are your hours?</dt>
+	<dd>We are open 24/7</dd>
+</dl>
+<script>
 	(function(){
-		var link = $('link');
-		$('button').click(function(){
-			var stylesheet = $(this).data('file');
-			console.log(stylesheet);
-			$(this).siblings('button').removeAttr('disabled').end().attr('disabled','disabled');
-			link.attr('href', stylesheet+'.css');
-		});
+		var dd = $('dd');
+
+		dd.filter(':nth-child(n+4)').addClass('hide');
+		$('dt').on('mouseenter', function(){
+			$(this)
+				.next()
+					.slideDown(300)
+						.siblings('dd')
+							.slideUp(300);
+			
+		})
 
 	})();
-	</script>
+</script>
 </body>
 </html>
