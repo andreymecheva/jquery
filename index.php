91885ea46f846543d07f1fbdd1fd1f707bcb6347
andreymecheva <andreymecheva@hotmail.com>
2014-03-06 09:41:01 -0500

    dos

--- index.php
+++ index.php
@@ -2,32 +2,27 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Jquery Tut</title>
-	<style>
-	ul.emphasis li{
-
-	}
-	</style>
+	<title>Jquery events</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	
+	<link rel="stylesheet" href="day.css">
 
 </head>
 <body>
 <div class="container">
 	<h1>Welcome to my website</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, quam, esse ex excepturi minus expedita eveniet odio vitae amet dicta! Totam quod soluta at iusto neque officiis rerum qui cumque.</p>
-	<ul class="emphasis">
-		<li>item</li>
-		<li>item2</li>
-		<li></li>
-	</ul>
+	<button data-file="day">Day</button>
+	<button data-file="night">Night</button>
+	<script>
+	(function(){
+		var link = $('link');
+		$('button').click(function(){
+			var stylesheet = $(this).data('file');
+			console.log(stylesheet);
+			$(this).siblings('button').removeAttr('disabled').end().attr('disabled','disabled');
+			link.attr('href', stylesheet+'.css');
+		});
 
-	<ul>
-		<li>hello from the nest</li>
-	</ul>
-</div>
-<script>
-	$('ul.emphasis').children('li').eq(2).prev().text('added with jquery');
-</script>
+	})();
+	</script>
 </body>
 </html>
