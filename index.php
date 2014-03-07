07946140c8dea5e1df44b9e985aad0af11381863
andreymecheva <andreymecheva@hotmail.com>
2014-03-07 09:13:52 -0500

    cinco

--- index.php
+++ index.php
@@ -8,15 +8,28 @@
 
 </head>
 <body>
-	<div class="container">
-<h2>Click me</h2>
+<article>
+	<h1>My asoweme Post</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, est, modi beatae omnis vel molestiae in temporibus laboriosam dolor fuga voluptatem officiis aspernatur expedita error soluta at aut facilis! Ipsum.</p>
+	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, dolorem veniam placeat accusantium tempora adipisci iusto eaque debitis aliquam ea quis nisi accusamus sed dignissimos natus reiciendis perferendis laboriosam sapiente!</p>
+</article>
+<blockquote style="float:left; background: #ccc; width:150px;">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, placeat, perferendis, harum, sequi nisi reiciendis porro alias impedit quod explicabo sit a ipsum sapiente illo beatae sint maiores minus repellendus.
+</blockquote>
+<div class="container">
+	
 </div>
 <script>
-$('div.container').delegate('h2','click', function(){
-	console.log('clicked');
-	$(this).clone().appendTo('body');
-});
+	(function(){
+		/*$('article').append('Hello from the javascript');*/
+		$('div.container').append('Hello from jquery into container');
 
+		$('<h2></h2>',{
+			text: 'Hello from JavaScript',
+			class: 'myClass'
+		}).appendTo('article');
+
+	})();
 </script>
 </body>
 </html>
